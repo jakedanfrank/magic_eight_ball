799ef4b2b5e4c77376a05789e53867c17ce1819e
Jake Frank <jacob423frank@gmail.com>
2019-08-20 13:35:34 -0600

    second commit

--- magic_eight_ball_OG.rb
+++ magic_eight_ball_OG.rb
@@ -15,75 +15,80 @@ require 'colorize'
 #-------------------------------------------------------------------------
 class Answers 
   def initialize
+
+    @answers = [
+      "No",
+      "Perhaps, depends on the day",
+      "Yes",
+      "Maybe",
+      "I hope not",
+      "Ask me tomorrow",
+      "Most definitely not",
+      "Abosolutely",
+      "It is certain",
+      "Ask again later",
+      "Very doubtful",
+      "Dont count on it",
+      "Sorry I wasn't listening"
+      ]
+
   main_menu
   end
 
   def main_menu
     puts "--- Welcome to the Magic Eight Ball! ---"
-    puts "1) Ask a question"
-    puts "2) Add a question"
-    puts "3) Show all answers"
-    puts "4) Exit"
+    puts "1) Ask a question".colorize(:blue)
+    puts "2) Add a question".colorize(:blue)
+    puts "3) Show all answers".colorize(:blue)
+    puts "4) Exit".colorize(:green)
     print ">"
     choice = gets.to_i
   case choice
     when 1
       ask_question
-      gets.strip 
+      # gets.strip 
     when 2
-      add_question
+      add_answer
     when 3
-      show_questions
+      show_answers
     when 4
-      puts  "Thanks for playing! Goodbye!"
-      exit
+      puts  "Thanks for playing! Goodbye!".colorize(:green)
+      exit!
     end
   end
  
     # main_menu
   def ask_question
-    puts "Ask your question"
+    puts "Ask your question".colorize(:blue)
+    puts 
     gets.strip
+    puts @answers.sample
+    puts 
     main_menu
   end
-
-  def add_question
-    puts "Add your question"
-    gets.strip 
+  
+  def add_answer
+    puts "Add your answer".colorize(:blue)
+    new_answer = gets.strip
+    # @new_answer = "Your mom"
+    @answers << new_answer 
+    puts "Hey we added *#{new_answer}* , to your answers."
+    puts
     main_menu 
   end
-
-  def show_questions
+  
+  def show_answers
     puts "Question List" #add array of questions
+    puts '--------------------'
+    puts
+    puts @answers 
+    puts
     main_menu
   end
   
 end
 
-Answers.new 
-      # main_menu
-      # @answer = [
-      # "No",
-      # "Perhaps, depends on the day"
-      # "Yes",
-      # "Maybe",
-      # "I hope not",
-      # "Ask me tomorrow",
-      # "Most definitely not",
-      # "Abosolutely",
-      # "It is certain",
-      # "Ask again later",
-      # "Very doubtful",
-      # "Dont count on it",
-      # "Sorry I wasn't listening"
-      # ]
-
-
-      # end
-      # end
-
-
-      # end
+Answers.new
 
 
 
